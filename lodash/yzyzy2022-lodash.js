245f94a18fe6df6d54f42a2b75f6087024bf1d3d
ww <1253059859@qq.com>
2022-09-15 21:15:43 +0800

    1

--- lodash/yzyzy2022-lodash.js
+++ lodash/yzyzy2022-lodash.js
@@ -12,16 +12,89 @@ var yzyzy2022 = {
         result.push(ary)
         result.shift()
         return result
-    }
-,
-    compact:function (arry) {
+    },
+
+    compact:function (ary) {
         var result = []
-        for (var i = 0; i < arry.length; i++) {
-            if (arry[i]) {
-                result.push(arry[i])
+        for (var i = 0; i < ary.length; i++) {
+            if (ary[i]) {
+                result.push(ary[i])
             }
         }
         return result
-    }
+    },
+    
 
+    drop: function (array,n) {
+        var result = []
+        if (n > array.length) {
+            return []
+        }
+        for (var i = 0; i < array.length; i++) {
+            if (i >= n) {
+                result.push(array[i])
+            } 
+        }
+        return result
+    },
+
+
+    fill: function (ary,vu,st = 0,ed = ary.length) {
+       
+        for (var i = st; i < ed; i++) {
+            
+                ary[i] = vu
+            
+            
+        }
+        return ary
+    },
+
+    
+    flatten: function (array) {
+        var result = []
+        for (var i = 0; i < array.length; i++) {
+            var it = array[i]
+            if (Array.isArray(it)) {
+                for (var j = 0; j < array.length; j++) {
+                    result.push(it[j])
+                }
+            } else {
+                result.push(it)
+            }
+        }
+        return result
+    },
+    
+    flattenDeep: function flattenDeep(array) {
+        var result = []
+        for (var i = 0; i < array.length; i++) {
+            
+            if (Array.isArray(array[i])) {
+                result  = result.concat(yzyzy2022.flattenDeep(array[i]))
+               
+            } else {
+                result  = result.concat(array[i])
+            }
+        }
+        return result
+    },
+
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
